7f1bb3b683e24fbbe948ace260d55c86171a5257
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-05 14:02:27 +0530

    Auth0 Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 function App() {
 	const [count, setCount] = useState(0);
 	const { isAuthenticated, user } = useAuth0();
+	console.log(user);
 	return <>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</>;
 }
 
