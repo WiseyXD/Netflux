34ba0ac29e2f99afd017bb5901ee22f0505b5201
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-09 23:00:39 +0530

    fix : Constants File Updated

--- src/Services/movieApi.jsx
+++ src/Services/movieApi.jsx
@@ -1,14 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { API_OPTIONS, baseUrl } from "../Utils/constants";
+import { baseUrl, API_TOKEN } from "../Utils/constants";
 
 // const createReq = (url) => ({ url, options: API_OPTIONS });
 
 export const movieApi = createApi({
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://api.themoviedb.org/3/",
+		baseUrl: baseUrl,
 		prepareHeaders: (headers, { getState }) => {
-			const token =
-				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODEzMTllYWVjZjUyMjU4YjI4ZDY3MWE0NjQzYTIzNiIsInN1YiI6IjY1NGJjNGVhMjg2NmZhMDBmZTAxYjMxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._h5PkIUYfbfD-bA_miksXaxjsUKNaFEzm5cNAU5FjVw";
+			const token = "Bearer " + API_TOKEN;
 			headers.set("Authorization", token);
 			return headers;
 		},
--- src/Utils/constants.jsx
+++ src/Utils/constants.jsx
@@ -1,10 +1,3 @@
-export const API_OPTIONS = {
-	method: "GET",
-	headers: {
-		accept: "application/json",
-		Authorization:
-			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDEzYjdmMzE5OTlkZTM5OGMzYzkwZDc3ZmQwMGU0MSIsInN1YiI6IjY1NGJjNGVhMjg2NmZhMDBmZTAxYjMxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bgIuP2VytB0kLYuOp1Cf5SVUUZ0VZTfLy4Itgri-EK4",
-	},
-};
-
-export const baseUrl = "https://api.themoviedb.org";
+export const baseUrl = "https://api.themoviedb.org/3/";
+export const API_TOKEN =
+	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODEzMTllYWVjZjUyMjU4YjI4ZDY3MWE0NjQzYTIzNiIsInN1YiI6IjY1NGJjNGVhMjg2NmZhMDBmZTAxYjMxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._h5PkIUYfbfD-bA_miksXaxjsUKNaFEzm5cNAU5FjVw";
