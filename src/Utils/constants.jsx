9e80a40b6aba415ff2e33ab117bc1e24d8594ff9
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-16 13:55:11 +0530

    bug : Issues with .env

--- src/Utils/constants.jsx
+++ src/Utils/constants.jsx
@@ -2,4 +2,4 @@ export const baseUrl = "https://api.themoviedb.org/3/";
 export const API_TOKEN =
 	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODEzMTllYWVjZjUyMjU4YjI4ZDY3MWE0NjQzYTIzNiIsInN1YiI6IjY1NGJjNGVhMjg2NmZhMDBmZTAxYjMxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._h5PkIUYfbfD-bA_miksXaxjsUKNaFEzm5cNAU5FjVw";
 
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = import.meta.env.OPENAI_API_KEY;
