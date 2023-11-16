134a3cd720298784670455c6fc15efb0169c7834
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-16 23:07:50 +0530

    bug : API Scammed

--- src/Utils/openai.jsx
+++ src/Utils/openai.jsx
@@ -2,6 +2,6 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./constants";
 
 export const openai = new OpenAI({
-	apiKey: OPENAI_KEY,
+	apiKey: "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",
 	dangerouslyAllowBrowser: true,
 });
